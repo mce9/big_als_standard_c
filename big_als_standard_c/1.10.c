e3aeff6c4b0006d4ba8c1cdfd88b3916c6c1dd61
David W <dwing@noncosi.com>
2020-01-18 05:07:31 -0800

    Add 1.10

---
+++ big_als_standard_c/1.10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+
+	printf("%-5c", 'A');
+	printf("%-5c", 'B');
+	printf("\n");
+	printf("%-5d", 55);
+	printf("%-5d", 1777);
+	printf("\n");
+	printf("%-5d", 600);
+	printf("%-5d", 451);
+	printf("\n");
+
+	return 0;
+}
