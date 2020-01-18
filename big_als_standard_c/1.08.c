7cccf2d6f4b47ee92f1f608ddf084f588ca52d0a
David W <dwing@noncosi.com>
2020-01-18 04:33:49 -0800

    Add 1.08.c

---
+++ big_als_standard_c/1.08.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("%s\n", "Goonies never say die");
+	printf("Klaata %s nikto\n", "barada");
+
+	/* character constants are enclosed in single quotes */
+
+	printf("%c%c, a bear in his natural habitat: a %s\n\n", 'A', 'h', "Studebaker");
+	printf("The value of PI is approximately %f\n", 3.14159);
+	printf("Euler's number is more or less equal to %f\n", 2.718282);
+
+	return 0;
+}
