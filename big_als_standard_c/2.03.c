22c747bf9d08486573953b9c7cb5a51ac61bdd89
mce9 <dwing@noncosi.com>
2020-01-31 20:20:08 +0100

    Add 2.03.c

---
+++ big_als_standard_c/2.03.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+
+	// declare a variable
+	int num;
+	// assign it a value
+	num = 1963;
+	// display it in a formatted string
+	printf("The value is %d\n", num);
+
+	return 0;
+}
