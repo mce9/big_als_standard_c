e2c601832d6df8adfeac9211f2c0755ceefbd7e0
mce9 <dwing@noncosi.com>
2020-02-06 21:50:58 +0100

    Add 2.06.c

---
+++ big_als_standard_c/2.06.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+
+	int abbott, costello;
+	abbott = costello = 1;
+
+	printf("Abbott is on %dst.\n", abbott);
+	printf("Costello is on %dst.\n", costello);
+
+	return 0;
+}
