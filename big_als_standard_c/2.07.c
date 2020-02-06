40bf93a9dfde0a7cd5790e302c4190dd04d28d89
mce9 <dwing@noncosi.com>
2020-02-06 21:54:23 +0100

    Add 2.07.c

---
+++ big_als_standard_c/2.07.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define SYMBOLIC_CONSTANT 177
+
+int main(void) {
+
+	int variable = 249;
+
+	printf("This is a literal constant: %d\n", 287);
+	printf("This is a symbolic constant: %d\n", SYMBOLIC_CONSTANT);
+	printf("This is a variable: %d\n", variable);
+
+	return 0;
+}
