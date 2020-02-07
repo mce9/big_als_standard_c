2ba1b637ea76bf4967d1ea43f78ee51a461bb7cb
mce9 <dwing@noncosi.com>
2020-02-07 21:44:12 +0100

    Add 2.08.c

---
+++ big_als_standard_c/2.08.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+
+	// declare x and y
+	int x;
+	int y;
+
+	// use the uninitialized variables
+	printf("The value of x is %d", x);
+	printf(" and the value of y is %d.", y);
+
+	return 0;
+}
