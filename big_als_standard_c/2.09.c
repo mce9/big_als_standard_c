5fd41fe0c67bf30bc47e0a25d418b157f58be2ac
mce9 <dwing@noncosi.com>
2020-02-07 21:47:06 +0100

    Add 2.09.c

---
+++ big_als_standard_c/2.09.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+
+	int sum;
+	/* 
+	the sum variable will hold 
+	the result of the expression
+	*/
+
+	sum = 50 + 50;
+
+	printf("The sum of 50 and 50 is %i\n", sum);
+
+	return 0;
+}
