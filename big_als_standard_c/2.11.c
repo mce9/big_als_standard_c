9757599298f3d982d479c496e1a2a6183180d4f9
mce9 <dwing@noncosi.com>
2020-02-27 20:19:31 +0100

    Add 2.11.c

---
+++ big_als_standard_c/2.11.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+
+	// declare variables
+	int value1, value2, product;
+
+	// assign the values
+	value1 = 42;
+	value2 = 73;
+	product = value1 * value2;
+
+	// display the values
+	printf("The product of %d and %d is %d\n", value1, value2, product);
+
+	return 0;
+}
