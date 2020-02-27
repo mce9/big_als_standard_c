16dd414c42905d7852adf9aa83167dbadb2d2ce7
mce9 <dwing@noncosi.com>
2020-02-27 20:15:00 +0100

    Add 2.10.c

---
+++ big_als_standard_c/2.10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+
+	int x;
+	int y;
+	int answer;
+
+	x = 5;
+	y = 7;
+	answer = 5 + 12;
+
+	printf("The answer is %d\n", answer);
+	answer = x + y;
+	printf("The answer is %d\n", answer);
+	answer = x - 2;
+	printf("The answer is %d\n", answer);
+	return 0;
+}
