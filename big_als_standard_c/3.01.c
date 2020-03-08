1a11a3d1fd7d5e722ee9aec3564377e453f14dd2
mce9 <dwing@noncosi.com>
2020-03-08 20:59:15 +0100

    Add 3.01.c

---
+++ big_als_standard_c/3.01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+
+	/*
+	declare integer and float variables
+	*/
+	int intVal;
+	float floatVal;
+
+	// initialize them
+	intVal = 3;
+	floatVal = 3.14;
+
+	printf("The value of the integer variable is %d", intVal);
+	printf("\nAnd the value of the float variable is %f\n", floatVal);
+
+	return 0;
+}
