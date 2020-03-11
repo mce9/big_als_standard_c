792bc03868231f7894fc8fb3eec51985b9b45b5b
mce9 <dwing@noncosi.com>
2020-03-11 22:27:20 +0100

    Add 3.02.c

---
+++ big_als_standard_c/3.02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+
+	int intVar = 5.4;
+	float floatVar = 5;
+
+	printf("Assign 5.4 to the integer variable intVar\n");
+	printf("intVar now contains the value %d\n", intVar);
+	printf("Assign 5 to the floating point variable floatVar\n");
+	printf("floatVar now contains the value %f\n", floatVar);
+
+	return 0;
+}
