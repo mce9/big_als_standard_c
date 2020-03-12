8655ae429f1dc456a566053ad63ba37e55cf6c2b
mce9 <dwing@noncosi.com>
2020-03-12 20:46:04 +0100

    Add 3.04.c

---
+++ big_als_standard_c/3.04.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+	float floatVar;
+	double doubleVar;
+	int integerVar;
+
+	floatVar = 104.5;
+	doubleVar = 123.009;
+	integerVar = 1000;
+
+	printf("floatVar is %f\n", floatVar);
+	printf("doubleVar is %f\n", doubleVar);
+	printf("%d is the value of integerVar\n", integerVar);
+
+	return 0;
+}
